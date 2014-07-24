75dd9e7f2368c31df674c8cfec2be217f3354e44
Dustin J. Mitchell <dustin@mozilla.com>
2014-07-24 16:42:57 -0400

    add help

--- fwunit/scripts.py
+++ fwunit/scripts.py
@@ -16,6 +16,23 @@ def prep():
             --route-xml: output of 'show route | display xml'
             --configuration-security-zones-xml: output of 'show configuration security zones | display xml'
         The output will be written to --output, defaulting to 'rules.pkl'
+
+        The output contains a list of Rule objects, where each Rule has `src`
+        and `dst`, IPSets consisting of the source and destination addresses to
+        which it applies; `app`, the name of the Juniper application to which
+        it applies, and `name`, the name of the juniper policy from which it
+        was derived.
+
+        The rules are normalized as follows:
+
+         - for a given source and destination IP and application, exactly 0 or 1 rules match;
+           stated differently, the in-order processing of policies is already "baked in"
+
+         - flow IPs are limited by the to- and from-zones of the original policy, so given a "from any"
+           policy with from-zone ABC, the resulting rule's `src` will be ABC's IP space, not 0.0.0.0/0.
+
+         - application "all" is translated into every application that appears anywhere in the config.
+
     """
     parser = argparse.ArgumentParser(
         description='Ingest output from a Juniper firewall and create a datafile containing the results.', epilog=epilog)
