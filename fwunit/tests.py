f0623ae8a2dba34c164343bd315308fa69cf7322
Dustin J. Mitchell <dustin@mozilla.com>
2014-08-29 17:37:35 -0400

    add colors to logs

--- fwunit/tests.py
+++ fwunit/tests.py
@@ -6,8 +6,10 @@ import cPickle as pickle
 import logging
 from fwunit.ip import IP, IPSet, IPPairs
 from nose.tools import ok_
+from blessings import Terminal
 
 log = logging.getLogger(__name__)
+terminal = Terminal()
 
 def _ipset(ip):
     if isinstance(ip, basestring):
@@ -36,7 +38,14 @@ class Rules(object):
             dst_matches = (rule.dst & dst)
             if not dst_matches:
                 continue
-            log.error("policy %s permits %s traffic from %s to %s" % (rule.name, app, src_matches, dst_matches))
+            log.error("policy {t.cyan}{name}{t.normal} permits {t.bold_cyan}{app}{t.normal} "
+                      "traffic\n{t.yellow}{src}{t.normal} -> {t.magenta}{dst}{t.normal}".format(
+                t=terminal,
+                name=rule.name,
+                app=app,
+                src=src_matches,
+                dst=dst_matches)
+                )
             failures += 1
         ok_(failures == 0, "%d matching rules" % failures)
 
@@ -51,11 +60,15 @@ class Rules(object):
             if rule.app != app:
                 continue
             if (rule.src & src) and (rule.dst & dst):
-                log.info("matched policy %s from %s to %s" % (rule.name, rule.src, rule.dst))
+                log.info("matched policy {t.cyan}{name}{t.normal}\n{t.yellow}{src}{t.normal} "
+                         "-> {t.magenta}{dst}{t.normal}".format(
+                    t=terminal, name=rule.name, src=rule.src, dst=rule.dst))
                 remaining = remaining - IPPairs((rule.src, rule.dst))
         if remaining:
-            flows = ",\n".join("%s -> %s" % p for p in remaining)
-            raise AssertionError("no rule found for flows, app %s\n%s" % (app, flows))
+            flows = ",\n".join("{t.yellow}{src}{t.normal} -> {t.magenta}{dst}{t.normal}".format(
+                                t=terminal, src=p[0], dst=p[1]) for p in remaining)
+            raise AssertionError("No rule found for flows, app {t.bold_cyan}{app}{t.normal}\n{flows}"
+                                 .format(t=terminal, app=app, flows=flows))
 
     def sourcesFor(self, dst, app, ignore_sources=None):
         """Return an IPSet with all sources for traffic to any IP in dst on
@@ -72,7 +85,9 @@ class Rules(object):
                 if ignore_sources:
                     src = src - ignore_sources
                 if src:
-                    log.info("matched policy %s from %s to %s" % (rule.name, src, rule.dst & dst))
+                    log.info("matched policy {t.cyan}{name}{t.normal}\n{t.yellow}{src}{t.normal} "
+                             "-> {t.magenta}{dst}{t.normal}".format(
+                                t=terminal, name=rule.name, src=src, dst=rule.dst & dst))
                     rv = rv + src
         return rv
 
@@ -92,8 +107,9 @@ class Rules(object):
             dst_matches = (rule.dst & dst)
             if not dst_matches:
                 continue
-            log.info("matched policy %s from %s to %s: %s" % (rule.name, src_matches,
-                                                              dst_matches, rule.app))
+            log.info("matched policy {t.cyan}{name}{t.normal} app {t.bold_cyan}{app}{t.normal}\n"
+                     "{t.yellow}{src}{t.normal} -> {t.magenta}{dst}{t.normal}".format(
+                        t=terminal, name=rule.name, src=src_matches, dst=dst_matches, app=rule.app))
             rv.add(rule.app)
         return rv
 
@@ -111,8 +127,9 @@ class Rules(object):
                 if ignore_sources:
                     src = src - ignore_sources
                 if src:
-                    log.info("matched policy %s from %s to %s: %s" % (rule.name, src,
-                                                                      rule.dst & dst, rule.app))
+                    log.info("matched policy {t.cyan}{name}{t.normal} app {t.bold_cyan}{app}{t.normal}\n"
+                             "{t.yellow}{src}{t.normal} -> {t.magenta}{dst}{t.normal}".format(
+                                t=terminal, name=rule.name, src=src, dst=rule.dst & dst, app=rule.app))
                     rv.add(rule.app)
         return rv
 
